c9d9575e3963969ff354333fd0f6468bcee518de
Everton Fernandes <evertonfernandes.dev@gmail.com>
2023-07-10 22:07:16 -0300

    feat: :sparkles: adding talwind to the project

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,23 +5,10 @@ interface AddTodoPayload {
     newTodo: string;
 }
 
-const todoSlicer = createSlice({
-    name: 'todo',
-    initialState: ['Fazer café', 'Estudar Redux', 'Novo estado'],
-
-    reducers: {
-        add: (state, action: PayloadAction<AddTodoPayload>) => {
-            state.push(action.payload.newTodo)
-        }
-    }
-})
-
 export const store = configureStore({
     reducer: {
-        todo: todoSlicer.reducer
     }
 })
 
-export const { add } = todoSlicer.actions
 export type RootState = ReturnType<typeof store.getState>
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
