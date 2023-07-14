313cc5b8ce0db88767fe5c7f1fa19a91af578075
Everton Fernandes <evertonfernandes.dev@gmail.com>
2023-07-13 21:48:42 -0300

    feat: :sparkles: finalizing redux structure

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,19 @@
-import { useAppSelector } from "../store"
+import { useAppSelector } from '../store';
+import { useCurrentLesson } from '../store/slices/player';
 
 export function Header() {
-  const { currentLesson, currentModule } = useAppSelector(state => {
-    const { currentLessonIndex, currentModuleIndex } = state.player
+  const { currentLesson, currentModule } = useCurrentLesson()
+  const isCourseLoading = useAppSelector(state => state.player.isLoading)
 
-    const currentModule = state.player.courser.modules[currentModuleIndex]
-    const currentLesson = currentModule.lessons[currentLessonIndex]
-    console.log(currentModule.title);
 
-    return { currentModule, currentLesson }
-  })
+  if(isCourseLoading) {
+   return <h1 className='text-2xl font-bold'>Carregando...</h1>
+  }
   return (
     <div className="flex flex-col gap-1">
 
-      <h1 className="text-2xl font-bold">{currentLesson.title}</h1>
-      <span className="text-sm text-zinc-400">Módulo "{currentModule.title}"</span>
+      <h1 className="text-2xl font-bold">{currentLesson?.title}</h1>
+      <span className="text-sm text-zinc-400">Módulo "{currentModule?.title}"</span>
     </div>
   )
 }
